269ba27632d75c1781821970c6f0ca02a9437d72
StefanoBelli <sa.belli@hotmail.it>
2017-07-01 17:18:39 +0200

    Preparing travis to deploy pages

--- scripts/deploy_pages.sh
+++ scripts/deploy_pages.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-REMOVE="src scripts README.md appveyor.yml .travis.yml sonar-project.properties build_wrapper .gitignore Doxyfile CMakeLists.txt LICENSE "
+REMOVE="lib src scripts README.md appveyor.yml .travis.yml sonar-project.properties build_wrapper .gitignore Doxyfile CMakeLists.txt LICENSE "
 
 make docs
 
+#cleanup dirty tree
 rm -rfv $REMOVE
+
+#keep html
+rm -r doc/latex 
+
+#move html folder
+mv doc/html/* . -v
