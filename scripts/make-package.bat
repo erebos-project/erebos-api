4ef88efac271d6d6836fc46c8290e67cce1136d9
Mattia Isgrò <matth_590@yahoo.it>
2017-07-02 17:22:58 +0200

    Complete make-package.bat

--- scripts/make-package.bat
+++ scripts/make-package.bat
@@ -1,26 +1,21 @@
 @echo off
 
-:usage_and_exit
-echo * Usage: %0 ^<version_major^> ^<version_minor^> ^<version_patch^> ^<target_arch^>
-exit /B 1
-
-
-: Configuration
-
 set API_TREE_MAIN_DIRECTORY=".."
 
 set API_VERSION_MAJOR=%1
 set API_VERSION_MINOR=%2
 set API_VERSION_PATCH=%3
 set API_TARGET_ARCH=%4
+set USE_MINGW=%5
 
-: NOT WORKING HERE
-if %API_VERSION_MAJOR% == "" goto usage_and_exit
-if %API_VERSION_MINOR% == "" goto usage_and_exit
-if %API_VERSION_PATCH% == "" goto usage_and_exit
-if %API_TARGET_ARCH% == ""   goto usage_and_exit
+: Check that arguments exist
+if "%API_TARGET_ARCH%" == "" (
+	goto usage_and_exit
+)
 
-set __REQUIRED__API_HEADER_VERSION_H="src\version.h"
+: Configuration
+
+set API_HEADER_VERSION_H="src\version.h"
 set API_HEADER_PATTERN="src\*.h"
 set API_LIBRARY_AR_EREBOS="lib\erebos.lib"
 set API_LIBRARY_SHARED_EREBOS="lib\erebos.dll"
@@ -33,11 +28,16 @@ set API_PACKAGE_LIBRARY_DIRECTORY=lib
 set API_PACKAGE_PLACE_README=1
 set API_PACKAGE_PLACE_LICENSE=1
 set API_PACKAGE_PLACE_AR=1
-set API_PACKAGE_PLACE_SHARED=1
+set API_PACKAGE_PLACE_SHARED=0
 set API_PACKAGE_PLACE_HEADERS=1
 set API_PACKAGE_NAME_PATTERN=%API_PACKAGE_NAME%-%API_VERSION_MAJOR%-%API_VERSION_MINOR%-%API_VERSION_PATCH%-windows-%API_TARGET_ARCH%
 
-set SHOULD_EXIT=0
+
+if "%USE_MINGW%" == "1" (
+	set API_LIBRARY_AR_EREBOS="lib\liberebos.a"
+	set API_LIBRARY_SHARED_EREBOS="lib\liberebos.so"
+)
+
 
 echo /!/ Starting dependency check...
 
@@ -76,37 +76,75 @@ if %API_PACKAGE_PLACE_SHARED% == 1 (
 	if exist %API_TREE_MAIN_DIRECTORY%/%API_LIBRARY_SHARED_EREBOS% (
 		echo ^>^>^> Shared library... found
 	) else (
+
 		echo !!! Shared library... not found
 		exit /B 1
 	)
 )
 
 echo --- (REQUIRED) Checking for generated version.h header...
-if not exist %API_TREE_MAIN_DIRECTORY%/%__REQUIRED__API_HEADER_VERSION_H% (
+if exist %API_TREE_MAIN_DIRECTORY%/%API_HEADER_VERSION_H% (
+	echo ^>^>^> version.h... found
+) else (
 	echo !!! version.h... not found
 	echo !!! Configure the build system in order to get the version.h header
+	exit /B 1
 )
 
-echo ^>^>^> version.h... found
-
-: powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('dest.zip', 'src'); }"
 
 echo /+/ Ready to build package...
+
+:set_compress_list
+
+	mkdir %API_PACKAGE_NAME_PATTERN%
+
+	if "%API_PACKAGE_PLACE_README%" == "1" (
+		copy /Y %API_TREE_MAIN_DIRECTORY%\%API_README% %API_PACKAGE_NAME_PATTERN%\
+	)
+
+	if "%API_PACKAGE_PLACE_LICENSE%" == "1" (
+		copy /Y %API_TREE_MAIN_DIRECTORY%\%API_LICENSE% %API_PACKAGE_NAME_PATTERN%\
+	)
+
+	mkdir %API_PACKAGE_NAME_PATTERN%\%API_PACKAGE_LIBRARY_DIRECTORY%
+
+	if "%API_PACKAGE_PLACE_AR%" == "1" (
+		copy /Y %API_TREE_MAIN_DIRECTORY%\%API_LIBRARY_AR_EREBOS% %API_PACKAGE_NAME_PATTERN%\%API_PACKAGE_LIBRARY_DIRECTORY%
+	)
+
+	if "%API_PACKAGE_PLACE_SHARED%" == "1" (
+		copy /Y %API_TREE_MAIN_DIRECTORY%\%API_LIBRARY_SHARED_EREBOS% %API_PACKAGE_NAME_PATTERN%\%API_PACKAGE_LIBRARY_DIRECTORY%
+	)
+
+	if "%API_PACKAGE_PLACE_HEADERS%" == "1" (
+		mkdir %API_PACKAGE_NAME_PATTERN%\%API_PACKAGE_INCLUDE_DIRECTORY%
+		copy /Y %API_TREE_MAIN_DIRECTORY%\%API_HEADER_PATTERN% %API_PACKAGE_NAME_PATTERN%\%API_PACKAGE_INCLUDE_DIRECTORY%
+	)
+
+echo /i/ Zipping everything...
 echo /i/ Using PowerShell to make a compressed package
 
-: set_compress_list
+if exist %API_PACKAGE_NAME_PATTERN%.zip (
+	del %API_PACKAGE_NAME_PATTERN%.zip
+)
 
-: echo /i/ Including the following:
-: print_compress_list
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.fileSystem'; [IO.Compression.Zipfile]::CreateFromDirectory('%API_PACKAGE_NAME_PATTERN%', '%API_PACKAGE_NAME_PATTERN%.zip'); }"
 
-: echo /i/ Zipping everything...
-: if ! zip -9 $API_PACKAGE_NAME_PATTERN $COMPRESS_LIST (
-: 	echo /-/ zip wasn't able to build your package
-: 	SHOULD_EXIT=1
-: )
-: echo /i/ Cleanup...
-: temporary_cleanup
+if errorlevel 1 (
+	echo /-/ Powershell wasn't able to build your package
+	rmdir /S/Q %API_PACKAGE_NAME_PATTERN%
+	exit /B 1
+)
 
-: echo /+/ Your package: %API_PACKAGE_NAME_PATTERN% was successfully built
-: exit $SHOULD_EXIT
+:temporary_cleanup
+echo /i/ Cleanup...
+: Delete temp dir
+rmdir /S/Q %API_PACKAGE_NAME_PATTERN%
 
+echo /+/ Your package: %API_PACKAGE_NAME_PATTERN% was successfully built
+
+exit /B 0
+
+:usage_and_exit
+echo * Usage: %0 ^<version_major^> ^<version_minor^> ^<version_patch^> ^<target_arch^> ^<use mingw^>
+exit /B 1
