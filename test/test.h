69cc8c91efc3a4478b7bbe8cd4e9571623c848a7
StefanoBelli <sa.belli@hotmail.it>
2017-07-31 13:34:41 +0200

    Trying new test from scratch

---
+++ test/test.h
@@ -0,0 +1,19 @@
+/*
+Unit testing macros,
+should be used with erebos framework only!
+*/
+#ifndef TEST_H
+#define TEST_H
+
+//Message for module
+
+//Prepare for function
+//Function callback
+//Result check
+//END TEST
+
+//Begin test 2 and so on...
+
+//End message for module
+
+#endif //TEST_H
