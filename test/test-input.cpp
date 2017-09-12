841ead4f4393c5f27b2c0030efc91b16881609a1
Mattia Isgrò <matth_590@yahoo.it>
2017-09-12 16:40:34 +0200

    Better empty tests for untestable modules

--- test/test-input.cpp
+++ test/test-input.cpp
@@ -1,1 +1,13 @@
-int main(){}
+#include "test.h"
+#include "framework.h"
+
+using namespace erebos;
+
+int main(int argc, char const *argv[]) {
+
+	NEW_MODULE_TEST("input");
+
+	END_MODULE_TEST();
+
+	return 0;
+}
