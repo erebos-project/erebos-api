069950b47ba96cd8db3afb00ff1c4ea30bb30007
StefanoBelli <sa.belli@hotmail.it>
2017-11-16 15:19:17 +0100

    Updated tests

--- test/test-time.cpp
+++ test/test-time.cpp
@@ -34,10 +34,6 @@ int main(int argc, char const *argv[]) {
 	CUSTOM_TEST_EQUALS(t.min,0);
 	CUSTOM_TEST_EQUALS(t.sec,0);
 
-	std::tm now;
-	localtime_r(&time_now, &now);
-	erebos::Time(&now);
-
 	END_TEST();
 
 	END_MODULE_TEST();
