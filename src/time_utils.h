56121cb4470b1a89a7cbc50d23a8dba90cdeacae
StefanoBelli <sa.belli@hotmail.it>
2017-11-16 15:11:01 +0100

    Added time tests

--- src/time_utils.h
+++ src/time_utils.h
@@ -5,52 +5,56 @@
 #ifndef EREBOS_TIME_UTILS_H
 #define EREBOS_TIME_UTILS_H
 
-#include <string>
-#include <ctime>
 #include "platform_defs.h"
+#include <ctime>
+#include <string>
 
 namespace erebos {
 
-	/*!
-	 * @brief contains year, month, day, hour, min, sec, refer to the constructor
-	 */
-	struct Time {
-	public:
-		int year;
-		int month;
-		int day;
-		int hour;
-		int min;
-		int sec;
+/*!
+ * @brief contains year, month, day, hour, min, sec, refer to the constructor
+ */
+struct Time {
+public:
+  int year;
+  int month;
+  int day;
+  int hour;
+  int min;
+  int sec;
 
-		/*!
-		 * @param right_now : tm structure
-		 * @brief parses right_now and then sets fields.
-		 * If nullptr, fields are initialized to 0
-		 */
-		ERAPI explicit Time(std::tm *right_now);
+  /*!
+   * @brief Creates empty Time instance
+   */
+  ERAPI inline Time() {}
 
-		/*!
-		 * @param format : A format string like: "YY/MM/DD @ hh:mm:ss"
-		 * @return new string containing formatted string
-		 * @brief formats are:
-		 *   - YY year
-		 *   - MM month
-		 *   - DD day
-		 *   - hh hour
-		 *   - mm minute
-		 *   - ss second
-		 */
-		ERAPI std::string to_string(std::string format) const;
+  /*!
+   * @param right_now : tm structure
+   * @brief parses right_now and then sets fields.
+   * If nullptr, fields are initialized to 0
+   */
+  ERAPI explicit Time(std::tm *right_now);
 
-	};
+  /*!
+   * @param format : A format string like: "YY/MM/DD @ hh:mm:ss"
+   * @return new string containing formatted string
+   * @brief formats are:
+   *   - YY year
+   *   - MM month
+   *   - DD day
+   *   - hh hour
+   *   - mm minute
+   *   - ss second
+   */
+  ERAPI std::string to_string(std::string format) const;
+};
 
-	/*!
-	 * @brief new Time-ready structure with current time
-	 * @return Time structure with current time values
-	 */
-	ERAPI Time get_localtime();
+/*!
+ * @brief new Time-ready structure with current time
+ * @return Time structure with current time values
+ */
+ERAPI Time get_localtime();
 
-}
+} // namespace erebos
 
 #endif
--- test/test-time.cpp
+++ test/test-time.cpp
@@ -1,11 +1,40 @@
+#include <sstream>
 #include "test.h"
-#include "framework.h"
+#include "time_utils.h"
 
 using namespace erebos;
 
 int main(int argc, char const *argv[]) {
 
 	NEW_MODULE_TEST("time");
+	
+	BEGIN_TEST PRE_CALL(erebos::get_localtime);
+
+	WITH_RETV CALLBACK();
+	CUSTOM_TEST_DISEQUALS(retv.year,0);
+	CUSTOM_TEST_DISEQUALS(retv.month,0);
+	CUSTOM_TEST_DISEQUALS(retv.day,0);
+	CUSTOM_TEST_DISEQUALS(retv.hour,0);
+	CUSTOM_TEST_DISEQUALS(retv.min,0);
+	CUSTOM_TEST_DISEQUALS(retv.sec,0);
+	
+	std::stringstream expected;
+	expected << retv.year << "/" << retv.month << "/"
+		<< retv.day << " @ " << retv.hour << ":" << retv.min
+		<< ":" << retv.sec;
+
+	CUSTOM_TEST_EQUALS(retv.to_string("YY/MM/DD @ hh:mm:ss"), expected.str());
+
+	Time t = Time();
+
+	CUSTOM_TEST_EQUALS(t.year,0);
+	CUSTOM_TEST_EQUALS(t.month,0);
+	CUSTOM_TEST_EQUALS(t.day,0);
+	CUSTOM_TEST_EQUALS(t.hour,0);
+	CUSTOM_TEST_EQUALS(t.min,0);
+	CUSTOM_TEST_EQUALS(t.sec,0);
+	
+	END_TEST();
 
 	END_MODULE_TEST();
 
