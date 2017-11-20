e89001ad216ca43804748f440339bace7748e98d
Mattia Isgrò <matth_590@yahoo.it>
2017-11-20 10:26:34 +0100

    Fix input tests

--- test/test-input.cpp
+++ test/test-input.cpp
@@ -11,33 +11,33 @@ int main(int argc, char const *argv[]) {
 
 #ifdef WINDOWS
 
-	BEGIN_TEST PRE_CALL(get_key);
+	BEGIN_TEST PRE_CALL(input::get_key);
 		// Empty test
 	END_TEST();
 
-	BEGIN_TEST PRE_CALL(fake_put);
+	BEGIN_TEST PRE_CALL(input::fake_put);
 		// Empty test
 	END_TEST();
 
 #elif defined(LINUX)
 
-	BEGIN_TEST PRE_CALL(getev_physical_keyboard);
+	BEGIN_TEST PRE_CALL(input::getev_physical_keyboard);
 		// Empty test
 	END_TEST();
 
-	BEGIN_TEST PRE_CALL(get_key);
+	BEGIN_TEST PRE_CALL(input::get_key);
 		// Empty test
 	END_TEST();
 
-	BEGIN_TEST PRE_CALL_OVERLOAD(fake_put, bool, const int&, const Key&, const KeyPressType&);
+	BEGIN_TEST PRE_CALL_OVERLOAD(input::fake_put, bool, const int&, const Key&, const KeyPressType&);
 		// Empty test
 	END_TEST();
 
-	BEGIN_TEST PRE_CALL_OVERLOAD(fake_put, bool, const int&, const Key&);
+	BEGIN_TEST PRE_CALL_OVERLOAD(input::fake_put, bool, const int&, const Key&);
 		// Empty test
 	END_TEST();
 
-	BEGIN_TEST PRE_CALL(destroy_virtual_kb_device);
+	BEGIN_TEST PRE_CALL(input::destroy_virtual_kb_device);
 		// Empty test
 	END_TEST();
 
